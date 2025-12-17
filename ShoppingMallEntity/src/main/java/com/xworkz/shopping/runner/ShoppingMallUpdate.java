c33c1869726248be3174b80cdf17f500ccdb7fa1
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-17 11:06:24 +0530

    UpdateQuery

--- ShoppingMallEntity/src/main/java/com/xworkz/shopping/runner/ShoppingMallUpdate.java
+++ ShoppingMallEntity/src/main/java/com/xworkz/shopping/runner/ShoppingMallUpdate.java
@@ -1,8 +1,10 @@
 package com.xworkz.shopping.runner;
+
 import com.xworkz.shopping.entity.ShoppingMall;
 
 import javax.persistence.*;
-public class ShoppingMallRunner {
+
+public class ShoppingMallUpdate {
     public static void main(String[] args) {
         ShoppingMall mall = new ShoppingMall();
         mall.setMallName("Phoenix Mall");
@@ -10,41 +12,43 @@ public class ShoppingMallRunner {
         mall.setShopsCount(250);
 
 
-        //    EntityManagerFactory emf = Persistence.createEntityManagerFactory("something");
-        //  EntityManager em = emf.createEntityManager();
-        //   EntityTransaction et = em.getTransaction();
-
-
-
         EntityManagerFactory emf = null;
         EntityManager em = null;
         EntityTransaction et = null;
 
-
-
-
-
-        try{
+        try {
             emf = Persistence.createEntityManagerFactory("something");
             System.out.println("EntityManagerFactory create : " + emf);
-            em = emf.createEntityManager(); // DML insert, update, delete, DQL select
+
+            em = emf.createEntityManager(); // DML insert, update, delete
             et = em.getTransaction();
             et.begin();
 
-            em.persist(mall);
-            System.out.println("shopping  inserted");
+             ShoppingMall entity = em.find(ShoppingMall.class, 1);
+            System.out.println("Old data : " + entity);
+
+             entity.setMallName("Phoenix Marketcity");
+            entity.setLocation("Chennai");
+            entity.setShopsCount(300);
+
+            ShoppingMall updatedEntity = em.merge(entity);
+            System.out.println("Updated data : " + updatedEntity);
+
             et.commit();
+            System.out.println("Shopping mall updated successfully");
+
         } catch (PersistenceException e) {
             e.printStackTrace();
-            et.rollback();
-        }finally {
-            if(emf !=null){
-                emf.close();}
-            if(em!=null){
+            if (et != null) {
+                et.rollback();
+            }
+        } finally {
+            if (em != null) {
                 em.close();
-
+            }
+            if (emf != null) {
+                emf.close();
             }
         }
-
     }
 }
